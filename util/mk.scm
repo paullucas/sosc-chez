06296df94c472e4be0d6b8c0951650f8e2c70503
Rohan Drape <rd@slavepianos.org>
2008-03-19 19:31:28 -0700

    Fix util/mk
    darcs-hash:335126e2bc59c5acdb9e1916f44bbcf8da97bbf4

--- util/mk.scm
+++ util/mk.scm
@@ -1,16 +1,31 @@
 (import (rhs r6rs rhs))
 (load "/home/rohan/sw/rhs/util/util.scm")
 
-(mk-r6rs '(sosc ikarus sosc)
+(mk-r6rs '(sosc r6rs sosc)
+	 '("/home/rohan/sw/sosc/src/sosc.scm")
+	 "/home/rohan/sw/sosc/r6rs/sosc.ss"
+	 '((rnrs base)
+	   (rnrs bytevectors)
+	   (rnrs io ports)
+	   (rnrs io simple)
+	   (rhs r6rs rhs))
+	 '()
+	 '())
+
+(mk-r6rs '(sosc ikarus transport)
 	 '("/home/rohan/sw/sosc/ikarus/ip.scm"
-	   "/home/rohan/sw/sosc/src/sosc.scm")
-	 "/home/rohan/sw/sosc/ikarus/sosc.scm"
+	   "/home/rohan/sw/sosc/src/transport.scm")
+	 "/home/rohan/sw/sosc/ikarus/transport.scm"
 	 '((rnrs base)
 	   (rnrs records syntactic)
 	   (rnrs bytevectors)
 	   (rnrs io ports)
 	   (rnrs io simple)
 	   (rhs r6rs rhs)
-	   (only (ikarus) parameterize tcp-connect))
+	   (sosc r6rs sosc)
+	   (only (ikarus) 
+		 parameterize 
+		 tcp-connect
+		 udp-open udp-connect udp-send udp-recv udp-close))
 	 '()
 	 '())
