df6e0990afe3fedccde672c7f59fa03453196442
Rohan Drape <rd@slavepianos.org>
2008-03-19 19:29:18 -0700

    Add support for local ikarus udp implementation
    darcs-hash:e3aaca197d6edfabf1ff817c45886f1fe86f7739

--- ikarus/ip.scm
+++ ikarus/ip.scm
@@ -1,26 +1,34 @@
+(define-record-type udp* (fields fd h p))
+
 ;; any -> bool
 (define udp:socket?
-  #f)
+  udp*?)
 
 ;; string -> int -> socket
 (define udp:open 
   (lambda (h p)
-    #f))
+    (let ((fd (udp-open)))
+      (udp-connect fd h p)
+      (make-udp* fd h p))))
 
 ;; socket -> bytevector -> ()
 (define udp:send 
   (lambda (u b)
-    #f))
+    (udp-send (udp*-fd u) b)))
 
 ;; socket -> maybe bytevector
 (define udp:recv
   (lambda (u)
-    #f))
+    (let* ((b (make-bytevector 8388608))
+	   (n (udp-recv (udp*-fd u) b))
+	   (r (make-bytevector n)))
+      (bytevector-copy! b 0 r 0 n)
+      r)))
 
 ;; socket -> ()
 (define udp:close 
   (lambda (u)
-    #f))
+    (udp-close (udp*-fd u))))
 
 (define-record-type tcp* (fields i o h p))
 
@@ -32,14 +40,16 @@
 (define tcp:open 
   (lambda (h p)
     (let-values
-     (((o i) (tcp-connect h p)))
+     (((o i) (tcp-connect h (number->string p))))
+     ;;(output-port-buffer-mode (buffer-mode none))
      (make-tcp* i o h p))))
 
 ;; socket -> bytevector -> ()
 (define tcp:send
   (lambda (fd b)
     (let ((o (tcp*-o fd)))
-      (put-bytevector o b))))
+      (put-bytevector o b)
+      (flush-output-port o))))
   
 ;; socket -> int -> maybe bytevector
 (define tcp:read
