23e7a157c5911d43d8603d079ebee83b8d7e2eb6
Rohan Drape <rd@slavepianos.org>
2008-09-27 16:26:28 -0700

    mk/mk.scm uses relative paths
    darcs-hash:746c6d3a39a37e4723aa1a2e02ff8a89e5c9ec90

--- mk/mk.scm
+++ mk/mk.scm
@@ -2,16 +2,16 @@
         (mk-r6rs))
 
 (mk-r6rs '(sosc)
-	 '("/home/rohan/sw/sosc/src/ikarus/ip.scm"
-           "/home/rohan/sw/sosc/src/bytevector.scm"
-	   "/home/rohan/sw/sosc/src/sosc.scm"
-	   "/home/rohan/sw/sosc/src/transport.scm")
-	 (string-append (list-ref (command-line) 1)
+         '("../src/ikarus/ip.scm"
+           "../src/bytevector.scm"
+           "../src/sosc.scm"
+           "../src/transport.scm")
+         (string-append (list-ref (command-line) 1)
                         "/sosc.ikarus.sls")
-	 '((rnrs)
+         '((rnrs)
            (rhs)
            (prefix (ikarus) ikarus:))
-	 '()
-	 '())
+         '()
+         '())
 
 (exit)
