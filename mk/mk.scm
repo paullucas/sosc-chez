9146fa46e95420d830acf90994061bb038d50755
Rohan Drape <rd@slavepianos.org>
2008-12-13 13:52:56 -0800

    initial pass at ypsilon support
    darcs-hash:5d934cc886889e44387d4ba3a6b6fadcfacf63d3

--- mk/mk.scm
+++ mk/mk.scm
@@ -14,4 +14,17 @@
          '()
          '())
 
+(mk-r6rs '(sosc)
+         '("../src/ypsilon/ip.scm"
+           "../src/bytevector.scm"
+           "../src/sosc.scm"
+           "../src/transport.scm")
+         (string-append (list-ref (command-line) 1)
+                        "/sosc.ypsilon.sls")
+         '((rnrs)
+           (rhs)
+           (prefix (socket) ypsilon:))
+         '()
+         '())
+
 (exit)
