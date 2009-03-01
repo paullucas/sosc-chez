8bb0dc9d01b0317058d144bfa966a19ba34184cd
Rohan Drape <rd@slavepianos.org>
2009-02-28 20:03:56 -0800

    delete ypsilon build rule
    darcs-hash:1cbfc6763cc35e2f41e52e24ec2265518dc90219

--- mk/mk.scm
+++ mk/mk.scm
@@ -14,17 +14,4 @@
          '()
          '())
 
-(mk-r6rs '(sosc)
-         '("../src/ypsilon/ip.scm"
-           "../src/bytevector.scm"
-           "../src/sosc.scm"
-           "../src/transport.scm")
-         (string-append (list-ref (command-line) 1)
-                        "/sosc.ypsilon.sls")
-         '((rnrs)
-           (rhs)
-           (prefix (socket) ypsilon:))
-         '()
-         '())
-
 (exit)
