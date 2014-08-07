aa470777658e16634d9c465e54ca3837932374bc
Rohan Drape <rd@slavepianos.org>
2014-08-07 16:19:16 -0700

    +src/ip.guile.scm (not working)
    Ignore-this: 11d18841efd97239f11691c475348384
    
    darcs-hash:29e019cc36c5f2cfb9f4110a454ca9fc0ad9e56e

--- mk/mk.scm
+++ mk/mk.scm
@@ -1,30 +1,28 @@
-(import (rnrs)
-        (mk-r6rs))
+(import (rnrs) (mk-r6rs))
+
+(define sosc-src
+  '("../src/bytevector.scm"
+    "../src/sosc.scm"
+    "../src/transport.scm"))
 
 (mk-r6rs '(sosc)
-         '("../src/ikarus/ip.scm"
-           "../src/bytevector.scm"
-           "../src/sosc.scm"
-           "../src/transport.scm")
-         (string-append (list-ref (command-line) 1)
-                        "/sosc.ikarus.sls")
-         '((rnrs)
-           (rhs)
-           (prefix (ikarus) ikarus:))
+         (cons "../src/ip.ikarus.scm" sosc-src)
+         (string-append (list-ref (command-line) 1) "/sosc.ikarus.sls")
+         '((rnrs) (rhs) (prefix (ikarus) ikarus:))
          '()
          '())
 
 (mk-r6rs '(sosc)
-         '("../src/plt/ip.ss"
-           "../src/bytevector.scm"
-           "../src/sosc.scm"
-           "../src/transport.scm")
-         (string-append (list-ref (command-line) 1)
-                        "/sosc.mzscheme.sls")
-         '((rnrs)
-           (rhs)
-           (prefix (scheme) plt:)
-           )
+         (cons "../src/ip.guile.scm" sosc-src)
+         (string-append (list-ref (command-line) 1) "/sosc.guile.sls")
+         '((rnrs) (rhs))
+         '()
+         '())
+
+(mk-r6rs '(sosc)
+         (cons "../src/ip.mzscheme.scm" sosc-src)
+         (string-append (list-ref (command-line) 1) "/sosc.mzscheme.sls")
+         '((rnrs) (rhs) (prefix (scheme) plt:))
          '()
          '())
 
